5708215e4560f733e558ef9ffe4551669a7026bd
Mutoi01 <pngando9@gmail.com>
2022-09-28 15:46:29 +0300

    commit tree 11917c706ab95309340a0d8bc7850f16cbb0e6f6

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+  validate :click_bait
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+
+  def click_bait
+    :title.include? ["Won't Believe","Secret","Top [number]","Guess"]
+  end
 end
